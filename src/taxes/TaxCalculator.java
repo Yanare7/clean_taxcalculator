684777fff45d7c38794b0f63ef2a3b3c112c67c0
rayanedahdah1@gmail.com <rayanedahdah1@gmail.com>
2024-10-29 15:50:23 +0100

    Little change of the name of the variables

--- src/taxes/TaxCalculator.java
+++ src/taxes/TaxCalculator.java
@@ -17,25 +17,25 @@ public class TaxCalculator {
 
 
     // Instance variables
-    private double income = 0; 
+    private double income = 0; //income in PLN 
     private char contractType = ' ';
     
     // social security taxes
-    private double socSecurity; 
-    private double socHealthSecurity; 
-    private double socSickSecurity; 
+    private double socialSecurity; 
+    private double socialHealthSecurity; 
+    private double socialSickSecurity; 
 
 
     private double taxDeductibleExpenses; 
 	private double taxedIncome; 
 	private double taxedIncomeRounded; 
     private double advanceTax; 
-    private double taxFreeIncome = 46.33; 
+    private double taxFreeIncome = 46.33; // tax-free income monthly 46,33 PLN
 	private double taxPaid; 
-    private double advanceTaxPaidRounded; 
-    private double socHealth1; 
-    private double socHealth2; 
     private double advanceTaxPaid; 
+    private double advanceTaxPaidRounded; 
+    private double socialHealth1; 
+    private double socialHealth2; 
 	private double netIncome; 
 	private DecimalFormat df00 = new DecimalFormat("#.00"); // Decimal format for two decimal places
     private DecimalFormat df = new DecimalFormat("#"); // Decimal format for whole numbers
@@ -105,45 +105,39 @@ public class TaxCalculator {
     //Calculate methods
 
 
-    private void calculateIncome() 
+    private void calculateSocialSecurity() 
     {
-        socSecurity = (income * SOCIAL_SECURITY_TAX_RATE) / 100; 
-        socHealthSecurity = (income * SOCIAL_HEALTH_SECURITY_TAX_RATE) / 100; 
-        socSickSecurity = (income * SOCIAL_SICK_SECURITY_TAX_RATE) / 100; 
+        socialSecurity = (income * SOCIAL_SECURITY_TAX_RATE) / 100; 
+        socialHealthSecurity = (income * SOCIAL_HEALTH_SECURITY_TAX_RATE) / 100; 
+        socialSickSecurity = (income * SOCIAL_SICK_SECURITY_TAX_RATE) / 100; 
     }    
 
 
 
-    private void calculateOtherTaxes() 
+    private void calculateHealthTaxes() 
     {
-        socHealth1 = (income * SOCIAL_HEALTH1_RATE) / 100; 
-        socHealth2 = (income * SOCIAL_HEALTH2_RATE) / 100; 
+        socialHealth1 = (income * SOCIAL_HEALTH1_RATE) / 100; 
+        socialHealth2 = (income * SOCIAL_HEALTH2_RATE) / 100; 
     }    
 
 
-    private void CalculateTaxDeductibleExpenses()
+    private void calculateTaxDeductibleExpenses()
     {
         if (contractType == 'E') {
-
+            taxDeductibleExpenses = 111.25;
         }
 		else if (contractType == 'C') {
             taxFreeIncome = 0;
+            taxDeductibleExpenses  = (income * TAX_DEDUCTIBLE_EXPENSES_RATE) / 100;
         }
-        taxDeductibleExpenses  = (income * TAX_DEDUCTIBLE_EXPENSES_RATE) / 100;
     }
-
-
-    
-	private void calculateTaxedIncome() 
-    {
-		taxedIncome = income - taxDeductibleExpenses; 
-        taxedIncomeRounded = Double.parseDouble(df.format(taxedIncome)); 
-	}
 
 
 
     private void calculateTax() 
     {
+        taxedIncome = income - taxDeductibleExpenses; 
+        taxedIncomeRounded = Double.parseDouble(df.format(taxedIncome)); 
         advanceTax = (taxedIncomeRounded * ADVANCE_TAX_RATE) / 100; 
     }
     
@@ -152,7 +146,7 @@ public class TaxCalculator {
     private void calculateAdvanceTax() 
     {
         taxPaid = advanceTax - taxFreeIncome; 
-        advanceTaxPaid = advanceTax - socHealth2 - taxFreeIncome; 
+        advanceTaxPaid = advanceTax - socialHealth2 - taxFreeIncome; 
 		advanceTaxPaidRounded = Double.parseDouble(df.format(advanceTaxPaid)); 
     }
 
@@ -160,7 +154,7 @@ public class TaxCalculator {
 
     private void calculateNetIncome() 
     {
-        netIncome = income - (socSecurity + socHealthSecurity + socSickSecurity + socHealth1 + advanceTaxPaidRounded);
+        netIncome = income - (socialSecurity + socialHealthSecurity + socialSickSecurity + socialHealth1 + advanceTaxPaidRounded);
     }
     
 
@@ -170,10 +164,9 @@ public class TaxCalculator {
 
     private void calculateCommonTaxes() 
     {
-        calculateIncome();
-        calculateOtherTaxes();
-        CalculateTaxDeductibleExpenses();
-        calculateTaxedIncome();
+        calculateSocialSecurity();
+        calculateHealthTaxes();
+        calculateTaxDeductibleExpenses();
         calculateTax();
         calculateAdvanceTax();
         calculateNetIncome();
@@ -188,7 +181,7 @@ public class TaxCalculator {
 
 
 
-	private void printChoice() 
+	private void printDetailsContract() 
     {
 		if (contractType == 'E') {
         	System.out.println("EMPLOYMENT CONTRACT");
@@ -201,11 +194,11 @@ public class TaxCalculator {
 
 
 
-	private void printSecurTaxes()
+	private void printSecurityTaxes()
     {
-		System.out.println("Social security tax: " + df00.format(socSecurity));
-        System.out.println("Health social security tax: " + df00.format(socHealthSecurity));
-        System.out.println("Sickness social security tax: " + df00.format(socSickSecurity));
+		System.out.println("Social security tax: " + df00.format(socialSecurity));
+        System.out.println("Health social security tax: " + df00.format(socialHealthSecurity));
+        System.out.println("Sickness social security tax: " + df00.format(socialSickSecurity));
         System.out.println("Income basis for health social security: " + income);
 	}
 
@@ -213,7 +206,7 @@ public class TaxCalculator {
 
 	private void printNewHealthSocialSecurTax() 
     {
-		System.out.println("Health social security tax: 9% = " + df00.format(socHealth1) + " 7.75% = " + df00.format(socHealth2));
+		System.out.println("Health social security tax: 9% = " + df00.format(socialHealth1) + " 7.75% = " + df00.format(socialHealth2));
 	
     }
 
@@ -232,7 +225,7 @@ public class TaxCalculator {
 	}
 
 
-	private void printCalculateTax1() 
+	private void printCalculateTax() 
     {
         System.out.println("Advance tax 18 % = " + advanceTax);
 
@@ -269,17 +262,16 @@ public class TaxCalculator {
 
     private void printAll() 
     {
-        printChoice();
-        printSecurTaxes();
+        printDetailsContract();
+        printSecurityTaxes();
         printNewHealthSocialSecurTax();
         printTaxedDeductible();
         printTaxedIncome();
-        printCalculateTax1();
+        printCalculateTax();
         printAlreadyPaidTax();
         printAdvancedTax();
         printNetIncome();
 
     }
-
 
 }
